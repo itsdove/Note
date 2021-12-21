38cf17caa59c177f4e1a61eb7bbf730d3e1125bb
KA002 <66370038+KA002@users.noreply.github.com>
2021-12-21 19:31:47 +0800

    12/21

---
+++ app/src/main/java/com/example/myapplication/MarksActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.databinding.ActivityMarksBinding;
+import com.example.myapplication.databinding.ActivityWebmarkBinding;
+
+public class MarksActivity extends AppCompatActivity {
+
+    //private AppBarConfiguration appBarConfiguration;
+    private ActivityMarksBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMarksBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+/*
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_webmark);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+*/
+        binding.FABADDMARKS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddDialog();
+            }
+        });
+    }
+    private void showAddDialog() {
+
+    }
+    /*
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_webmark);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+*/
+
+    }
---
+++ app/src/main/java/com/example/myapplication/ToolsActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ToolsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tools);
+
+        CardView cardView_webmark=findViewById(R.id.BUTTON_WEBMARK);
+        cardView_webmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ToolsActivity.this,WebmarkActivity.class);
+                startActivity(intent);
+            }
+        });
+        CardView cardView_marks=findViewById(R.id.BUTTON_MARKS);
+        cardView_marks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ToolsActivity.this,MarksActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
